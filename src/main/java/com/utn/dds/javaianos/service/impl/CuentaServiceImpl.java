5c9e3a60fee7198a50856e7f02e99cba8834f32f
Gaboxxz <gaboxxz@gmail.com>
2017-05-31 16:38:48 -0300

    arreglando

--- src/main/java/com/utn/dds/javaianos/service/impl/CuentaServiceImpl.java
+++ src/main/java/com/utn/dds/javaianos/service/impl/CuentaServiceImpl.java
@@ -1,33 +1,59 @@
 package com.utn.dds.javaianos.service.impl;
+
 import com.utn.dds.javaianos.domain.Cuenta;
 import com.utn.dds.javaianos.repository.CuentaRepository;
 import com.utn.dds.javaianos.service.CuentaService;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-/* 
-    Realiza la carga desde un archivo dejando una lista de objetos Cuenta en memoria.
-    En caso de excepcion genera un archivo log en formato JSON
-*/
 @Service
 public class CuentaServiceImpl implements CuentaService {
+
     @Autowired
     private CuentaRepository cuentaRepository;
-    
+
     @Override
     public void saveCuentas(MultipartFile file) {
-       cuentaRepository.saveCuentas(file);
+
+        String currentLine;
+
+        File convFile = new File(file.getOriginalFilename());
+        try {
+            file.transferTo(convFile);
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(convFile));
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(currentLine, ";");
+
+                Cuenta cuenta = new Cuenta();
+                cuenta.setNombre(st.nextToken());
+                cuenta.setEmpresa(st.nextToken());
+                cuenta.setValor(Double.parseDouble(st.nextToken()));
+                cuenta.setPeriodo(Integer.parseInt(st.nextToken()));
+
+                cuentaRepository.save(cuenta);
+            }
+
+        } catch (IOException ex) {
+            try {
+                throw ex;
+            } catch (IOException ex1) {
+                Logger.getLogger(CuentaServiceImpl.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
     }
-//    @Override
-//    public void saveDBCuentas(MultipartFile file){
-//        cuentaRepository.saveDBCuentas(file);
-//    }
-    
+
     @Override
     public List<Cuenta> getAllCuentas() {
         return cuentaRepository.getAllCuentas();
     }
 }
-

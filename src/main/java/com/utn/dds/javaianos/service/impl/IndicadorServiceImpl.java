097d4d331e1a0b83d4378ad1ba3e269bc8bb8287
Manuel Tuero <manutuero@gmail.com>
2017-07-03 22:23:32 -0300

    se modifica el front de modal evaluar indicador

--- src/main/java/com/utn/dds/javaianos/service/impl/IndicadorServiceImpl.java
+++ src/main/java/com/utn/dds/javaianos/service/impl/IndicadorServiceImpl.java
@@ -10,6 +10,7 @@ import com.utn.dds.javaianos.service.IndicadorService;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,20 +25,13 @@ public class IndicadorServiceImpl implements IndicadorService {
     @Autowired
     private CuentaRepository cuentaRepository;
     
-    /* @Override
-    public Double calcularValor(Indicador indicador) {
+    // faltan periodo y empresa
+    @Override
+    public Double obtenerValor(Indicador indicador) {
         Double valor = null;
         
-        // regex: uso expresiones regulares para descomponer el String con caracteres de escape (\\)
-        String[] sComponentes = indicador.getFormula().replaceAll("\\s|\\(|\\)","").split("\\+|-|\\*|/");
-        
-        List <Componente> componentes = new <Componente> ArrayList();
-        
-        for(String componente : sComponentes) {
-        
-        }
         return valor;
-    }*/
+    }
     
     @Override
     public Integer saveIndicador(Indicador indicador) {
@@ -83,5 +77,10 @@ public class IndicadorServiceImpl implements IndicadorService {
             (cuentaRepository.findFirstByNombre(componente) == null);
         
         return componentes.noneMatch(predicate);
+    }
+
+    @Override
+    public List<Indicador> getAllIndicadores() {
+        return indicadorRepository.findAll();
     }
 }

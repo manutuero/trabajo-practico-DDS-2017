2d273be82d1a827baf957d73a21d8f3071f99e82
Manuel Tuero <manutuero@gmail.com>
2017-07-11 17:51:26 -0300

    modulos 1 y 2 funcando (Cuentas y Consultar valores)

--- src/main/java/com/utn/dds/javaianos/service/impl/CotizacionServiceImpl.java
+++ src/main/java/com/utn/dds/javaianos/service/impl/CotizacionServiceImpl.java
@@ -9,9 +9,11 @@ import com.utn.dds.javaianos.repository.CuentaRepository;
 import com.utn.dds.javaianos.repository.EmpresaRepository;
 import com.utn.dds.javaianos.service.CotizacionService;
 import java.io.IOException;
+import java.util.List;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -47,6 +49,7 @@ public class CotizacionServiceImpl implements CotizacionService {
         }
     }
 
+    /* Inicio metodos del patron composite */
     @Override
     public void add(Componente componente) {
         // metodo no implementado en nodo hoja (leaf)
@@ -63,13 +66,27 @@ public class CotizacionServiceImpl implements CotizacionService {
         return null;
     }
 
+    /* Fin metodos del patron composite */
+
     @Override
     public Cotizacion buscarCotizacion(Cuenta cuenta, Empresa empresa, Integer periodo) {
-        return cotizacionRepository.findByCuentaAndEmpresaAndPeriodo((Cuenta)cuenta, empresa, periodo);
+        return cotizacionRepository.findByCuentaAndEmpresaAndPeriodo((Cuenta) cuenta, empresa, periodo);
+    }
+
+    @Override
+    public List<Cotizacion> getFilteredCotizaciones(Empresa empresa, Integer periodo) {
+        List<Cotizacion> cotizaciones = cotizacionRepository.findAll();
+        return cotizaciones
+                .stream()
+                .filter((Cotizacion cotizacion) -> cotizacion.getEmpresa().getNombre()
+                        .equals(empresa.getNombre())&& 
+                        cotizacion.getPeriodo()
+                        .equals(periodo))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Double calcularValor(Componente cuenta, Empresa empresa, Integer periodo) {
-        return this.buscarCotizacion((Cuenta)cuenta, empresa, periodo).getValor();
+        return this.buscarCotizacion((Cuenta) cuenta, empresa, periodo).getValor();
     }
 }

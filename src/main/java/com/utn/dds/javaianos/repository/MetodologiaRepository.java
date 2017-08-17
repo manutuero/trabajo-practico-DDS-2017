bda670426c158e233296938709fcd4f984b67a40
ignaciocordo <ignaciocordorusso@hotmail.com>
2017-08-16 21:06:01 -0300

    problema de tablas resuelto

--- src/main/java/com/utn/dds/javaianos/repository/MetodologiaRepository.java
+++ src/main/java/com/utn/dds/javaianos/repository/MetodologiaRepository.java
@@ -11,8 +11,10 @@ import org.springframework.stereotype.Repository;
 public interface MetodologiaRepository extends JpaRepository<Metodologia, Long> {
     //public Metodologia findByNombre(String nombre);
     public Metodologia findByCodigo(String codigo);
+    
     //@Query("INSERT INTO MetodologiaCondicion (condicion, metodologia) VALUES (:codCondicion, :codMetodologia)")
     //public void guardarCondicionesPorMetodologia(String codCondicion, String codMetodologia);
+    
     
     
 }
